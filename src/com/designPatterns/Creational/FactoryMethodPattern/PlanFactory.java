b924aa5ff563c1a83acfbc26ea28b4b9429a8c7b
subodhahansana@gmail.com <subodhahansana@gmail.com>
2019-06-09 16:21:50 +0530

    add

---
+++ src/com/designPatterns/Creational/FactoryMethodPattern/PlanFactory.java
@@ -0,0 +1,15 @@
+package com.designPatterns.Creational.FactoryMethodPattern;
+
+public class PlanFactory {
+
+	public static Plan getPlan(String planType) {
+
+		if (planType.equals("domestic")) {
+			return new DomesticPlan();
+		} else if (planType.equals("comercial")) {
+			return new CommersialPlan();
+		}
+
+		return null;
+	}
+}
