cdfcd6b696ce15a1891c208113ccd742ca7c0022
subodhahansana@gmail.com <subodhahansana@gmail.com>
2019-09-22 22:37:33 +0530

    added

---
+++ src/main/java/com/designPatterns/Creational/SingletonPattern/thread-safe-singleton/double-checked locking/getInstanceUsingDoubleLocking.java
@@ -0,0 +1,12 @@
+public class getInstanceUsingDoubleLocking{
+public static ThreadSafeSingleton getInstanceUsingDoubleLocking(){
+    if(instance == null){
+        synchronized (ThreadSafeSingleton.class) {
+            if(instance == null){
+                instance = new ThreadSafeSingleton();
+            }
+        }
+    }
+    return instance;
+}
+}
